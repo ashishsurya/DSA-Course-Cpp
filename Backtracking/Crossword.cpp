0f379578d1b32422cb576d6d9de3a99ca565a50e
ashishsurya <ashishsurya126@gmail.com>
2022-06-25 16:06:22 +0530

    Crossword completed

--- Backtracking/Crossword.cpp
+++ Backtracking/Crossword.cpp
@@ -1,16 +1,109 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool isValidVertical(char crossword[10][10], string word, int row, int col)
+{
+  if (10 - row < word.size())
+    return false;
+
+  for (int i = 0; i < word.size(); i++)
+  {
+    if (crossword[i + row][col] == '-' || crossword[i + row][col] == word[i])
+    {
+      continue;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool isValidHorizontal(char crossword[10][10], string word, int row, int col)
+{
+  if (10 - col < word.size())
+    return false;
+
+  for (int i = 0; i < word.size(); i++)
+  {
+
+    if (crossword[row][i + col] == '-' || crossword[row][i + col] == word[i])
+    {
+      continue;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+vector<bool> setVertical(char crossword[10][10], string word, int row, int col)
+{
+  vector<bool> placed(word.size(), false);
+  for (int i = 0; i < word.size(); i++)
+  {
+    if (crossword[i + row][col] == '-')
+    {
+      crossword[i + row][col] = word[i];
+      placed[i] = true;
+    }
+  }
+
+  return placed;
+}
+
+void resetVertical(char crossword[10][10], vector<bool> placed, int row, int col)
+{
+  for (int i = 0; i < placed.size(); i++)
+  {
+    if (placed[i])
+    {
+      crossword[i + row][col] = '-';
+    }
+  }
+}
+
+vector<bool> setHorizontal(char crossword[10][10], string word, int row, int col)
+{
+  vector<bool> placed(word.size(), false);
+
+  for (int i = 0; i < word.size(); i++)
+  {
+    if (crossword[row][i + col] == '-')
+    {
+      crossword[row][i + col] = word[i];
+      placed[i] = true;
+    }
+  }
+
+  return placed;
+}
+
+void resetHorizontal(char crossword[10][10], vector<bool> placed, int row, int col)
+{
+  for (int i = 0; i < placed.size(); i++)
+  {
+    if (placed[i])
+    {
+      crossword[row][i + col] = '-';
+    }
+  }
+}
+
 void completeCrossWord(char crossword[10][10], vector<string> words, int index)
 {
   // base case
-  if (index >= words.size())
+  if (index == words.size())
   {
     for (int i = 0; i < 10; i++)
     {
       for (int j = 0; j < 10; j++)
       {
-        cout << crossword[i][j] << " ";
+        cout << crossword[i][j] << "";
       }
       cout << endl;
     }
@@ -19,10 +112,28 @@ void completeCrossWord(char crossword[10][10], vector<string> words, int index)
 
   for (int i = 0; i < 10; i++)
   {
-    for (int j = 0; j < 10;j++) {
+    for (int j = 0; j < 10; j++)
+    {
+      if (crossword[i][j] == '-' || crossword[i][j] == words[index][0])
+      {
+        if (isValidHorizontal(crossword, words[index], i, j))
+        {
+          vector<bool> placed = setHorizontal(crossword, words[index], i, j);
+          completeCrossWord(crossword, words, index + 1);
+          resetHorizontal(crossword, placed, i, j);
+        }
 
+        if (isValidVertical(crossword, words[index], i, j))
+        {
+          vector<bool> placed = setVertical(crossword, words[index], i, j);
+          completeCrossWord(crossword, words, index + 1);
+          resetVertical(crossword, placed, i, j);
+        }
+      }
     }
   }
+
+  return;
 }
 
 int main()
@@ -54,7 +165,15 @@ int main()
     }
   }
 
+  words.push_back(s);
+
   completeCrossWord(crossword, words, 0);
+
+  // cout << input << endl;
+
+  // for(auto s:words) {
+  //   cout << s << endl;
+  // }
 
   return 0;
 }
